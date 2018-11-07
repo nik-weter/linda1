8cf5dff45ffe8e9bd10377e6053faf79a8d0a6f2
savelev <nik-weter@yandex.ru>
2018-11-07 12:40:49 +0700

    Email share done

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -21,4 +21,5 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     re_path(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<post>[-\w]+)/$', views.post_detail, name='post_detail'),
+    re_path(r'^(?P<post_id>\d+)/share/$', views.post_share, name="post_share"),
 ]
