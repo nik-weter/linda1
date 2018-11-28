9961284653e3de3043f23cadde091c407c05952b
nik-weter <nik-weter@yandex.ru>
2018-11-28 23:05:21 +0700

    3 part done

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path
 from . import views
+from .feeds import LatestPostFeds
 
 app_name = 'blog'
 urlpatterns = [
@@ -23,4 +24,5 @@ urlpatterns = [
     re_path(r'^tag/(?P<tag_slug>[-\w]+)/$', views.post_list, name='post_list_by_tag'),
     re_path(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<post>[-\w]+)/$', views.post_detail, name='post_detail'),
     re_path(r'^(?P<post_id>\d+)/share/$', views.post_share, name="post_share"),
+    re_path(r'^feeds/$', LatestPostFeds(), name='post_feed'),
 ]
